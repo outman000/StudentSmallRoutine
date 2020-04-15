cd6bc5f82a8f6c5b6b34871863d7ea10443a6997
704718841 <704718841@qq.com>
2020-04-15 23:30:51 +0800

    教职工增删改查

---
+++ Dto.IService/SmallRoutine/IFacultystaffService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ViewModel.SmallRoutine.MiddelViewModel;
+using ViewModel.SmallRoutine.PublicViewModel;
+using ViewModel.SmallRoutine.RequestViewModel;
+namespace Dto.IService.SmallRoutine
+{
+    public interface IFacultystaffService
+    {
+        //添加教职工信息 
+        BaseViewModel addFacultystaffInfo(FacultystaffBaseModel model);
+
+
+        //根据 id 删除 教职工信息
+        BaseViewModel delFacultystaffInfo(int id);
+
+
+        //修改 教职工信息
+        BaseViewModel updateFacultystafffo(FacultystaffMiddle student);
+
+
+        //根据条件查询
+        List<FacultystaffMiddle> GetListByParas(FacultystaffSearchModel model);
+    }
+}
---
+++ ViewModel/SmallRoutine/MiddelViewModel/FacultystaffMiddle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ViewModel.SmallRoutine.MiddelViewModel
+{
+    public class FacultystaffMiddle
+    {
+        public int id;
+        //姓名
+        public string Name { get; set; }
+        //性别
+        public string Sex { get; set; }
+        //出生年月
+        public DateTime? Birthday { get; set; }
+        //学校标识
+        public string SchoolCode { get; set; }
+        //学校名称
+        public string SchoolName { get; set; }
+        //部门标识
+        public string DepartCode { get; set; }
+        //部门
+        public string DepartName { get; set; }
+        //岗位标识
+        public string StaffCode { get; set; }
+        //岗位
+        public string StaffName { get; set; }
+        //国籍
+        public string Country { get; set; }
+        //身份证号/护照号
+        public string IdNumber { get; set; }
+        //户籍地址
+        public string PermanentAddress { get; set; }
+        //创建时间
+        public DateTime? CreateDate { get; set; }
+    }
+}
