7ec985ca3d7b94c074c8f65e08b1948bfa7c91c5
张祎荻 <479663032@qq.com>
2020-05-15 09:57:02 +0800

    添加istatisticsService

--- SmallRoutine/Controllers/StatisticsController.cs
+++ SmallRoutine/Controllers/StatisticsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Dto.IService.SmallRoutine;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,11 +12,11 @@ namespace SmallRoutine.Controllers
     [ApiController]
     public class StatisticsController : ControllerBase
     {
-        private readonly IStatisticsService StatisticsService;
+        private readonly IStatisticsService   istatisticsService;
 
         public StatisticsController(IStatisticsService statisticsService)
         {
-            StatisticsService = statisticsService;
+            istatisticsService = statisticsService;
         }
     }
 }
