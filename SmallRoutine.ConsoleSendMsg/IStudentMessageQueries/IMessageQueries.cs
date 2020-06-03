0d88846fc43b1172e03fac4c71642c8a82b6e938
704718841 <704718841@qq.com>
2020-06-03 20:26:13 +0800

    消息推送更新

--- SmallRoutine.ConsoleSendMsg/IStudentMessageQueries/IMessageQueries.cs
+++ SmallRoutine.ConsoleSendMsg/IStudentMessageQueries/IMessageQueries.cs
@@ -5,13 +5,32 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
-
+using ViewModel.SmallRoutine.RequestViewModel;
 
 namespace SmallRoutine.ConsoleSendMsg.IStudentMessageQueries
 {
     public interface IMessageQueries
     {
         //7:30 提醒未进行健康信息情况填报的教职员工和学生家长 填写体温等健康信息
-        List<string> GetListsBeforeToSchool();
+        List<BindMsgModel> GetListsBeforeToSchool();
+
+
+        //学生 7:30 提醒未进行健康信息情况填报的 学生家长 填写体温等健康信息
+        void GetListsXSBeforeToSchoolSend();
+
+
+        //学生 7:30 提醒未进行健康信息情况填报的 教职工 填写体温等健康信息
+        void GetListsJZGBeforeToSchoolSend();
+
+
+        //教职工  9：30 提醒未进行晨检情况填报的 教职工 填写体温等健康信息
+        void GetListsJZGMorningSend();
+
+
+        //教职工  13：30 提醒未进行午检情况填报的 教职工 填写体温等健康信息
+        void GetListsJZGNoonSend();
+
+
+        string GetPage(string posturl, string postData);
     }
 }
---
+++ ViewModel/SmallRoutine/MiddelViewModel/SendMessageModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ViewModel.SmallRoutine.MiddelViewModel
+{
+    public  class SendMessageModel
+    { //接收者（用户）的 openid
+        public string touser { get; set; }
+        //所需下发的订阅模板id
+        public string template_id { get; set; }
+        //模板跳转链接（海外帐号没有跳转能力）非必填
+        public string url { get; set; }
+        //跳小程序所需数据，不需跳小程序可不用传该数据，格式形如 { "appid": "", "pagepath": "" }
+        public object miniprogram { get; set; }
+        //模板内容，格式形如 { "key1": { "value": any,"color":"#173177" }, "key2": { "value": any,"color":"#173177"  } }
+        public object data { get; set; }
+    }
+}
